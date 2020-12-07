0a99cb8ca77309b93d64278b063b5ea4c54088a8
chrisjose <christopherjose707@gmail.com>
2020-12-07 15:05:25 -0800

    Backend signup, login, and logout complete

--- Soundcheck/app/controllers/application_controller.rb
+++ Soundcheck/app/controllers/application_controller.rb
@@ -6,30 +6,27 @@ class ApplicationController < ActionController::Base
     private
 
     def current_user
-        return nil unless session[:session_token]
-        @current_user ||= User.find_by(session_token: session[:session_token])
+      @current_user ||= User.find_by(session_token: session[:session_token])
     end
-
-    def logged_in?
-        !!current_user
+    
+    def login!(user)
+      @current_user = user
+      session[:session_token] = user.reset_session_token!
     end
-
-    def login(user)
-        user.reset_session_token!
-        session[:session_token] = user.session_token
-        @current_user = user
-    end
-
-    def logout
-        current_user.reset_session_token!
-        session[:session_token] = nil
-        @current_user = nil
-    end
-
-    def require_logged_in
-        unless current_user
+    
+    def required_logged_in
+      unless current_user
         render json: { base: ['invalid credentials'] }, status: 401
-        end
+      end
     end
-
+    
+    def logged_in?
+      !!current_user
+    end
+    
+    def logout!
+      current_user.reset_session_token! if logged_in?
+      session[:session_token] = nil
+      @current_user = nil
+    end
 end
