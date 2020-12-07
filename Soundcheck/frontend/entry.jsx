6190d2f678e736c77934c68bc9bc71b02739d99d
chrisjose <christopherjose707@gmail.com>
2020-12-07 15:51:07 -0800

    Finish session actions

--- Soundcheck/frontend/entry.jsx
+++ Soundcheck/frontend/entry.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import Root from './components/root';
 // import configureStore from './store/store';
+import * as SessionAPIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    //test start
+    window.login = SessionAPIUtil.login;
+    window.signup = SessionAPIUtil.signup;
+    window.logout = SessionAPIUtil.logout;
+    //test end
     
     const root = document.getElementById("root");
     ReactDOM.render(<h1>React is Working</h1>, root)
