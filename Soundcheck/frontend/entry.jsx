e995896bc50e8d302a6cb85a20e3e0774a2712b2
chrisjose <christopherjose707@gmail.com>
2020-12-07 16:42:59 -0800

    Finish session reducers

--- Soundcheck/frontend/entry.jsx
+++ Soundcheck/frontend/entry.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 import * as SessionAPIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    const store = configureStore();
     //test start
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     window.login = SessionAPIUtil.login;
     window.signup = SessionAPIUtil.signup;
     window.logout = SessionAPIUtil.logout;
