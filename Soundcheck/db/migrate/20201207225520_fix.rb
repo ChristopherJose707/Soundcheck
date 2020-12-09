49c837365752100cae388f808768bcf7175681a5
chrisjose <christopherjose707@gmail.com>
2020-12-08 16:45:43 -0800

    Delete Duplicate soundcheck

--- Soundcheck/db/migrate/20201207225520_fix.rb
+++
@@ -1,7 +0,0 @@
-class Fix < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :display_name
-    add_column :users, :display_name, :string
-    add_index :users, :display_name
-  end
-end
